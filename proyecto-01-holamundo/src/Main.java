b172fd66f67f04a47c56add22101099384f378d6
dulce canseco <20253ds119@utez.edu.mx>
2026-01-13 22:42:57 -0600

    Proyecto 01: Hola mundo

---
+++ proyecto-01-holamundo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Luis Rodriguez");
+        System.out.println("Grupo: 2D");
+        System.out.println("Fecha: 13/01/2026");
+        System.out.println("Hola Mundo");
+    }
+}
